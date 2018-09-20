a16154c8713cc06f66cf0a07087966df6428119d
Nilansh Bansal <bansalnilansh@gmail.com>
2018-09-20 16:44:31 +0530

    changes

--- reddit_flask_api/app.py
+++ reddit_flask_api/app.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import argparse
-import csv 
+import csv
 import urllib
 
 app = Flask(__name__)
@@ -13,18 +13,21 @@ def home():
     searchQuery = request.args.get('q')
     if searchQuery is None:
         searchQuery = 'ico scam'
-    subreddit = request.args.get('r')
+    subreddit = request.args.get('s')
     if subreddit is None:
         subreddit = 'ethtrader'
     limit = request.args.get('l')
     if limit is None:
         limit = 25
-    url = 'https://api.pushshift.io/reddit/search/submission/?q=' + urllib.parse.quote_plus(searchQuery) + '&subreddit=' + urllib.parse.quote_plus(subreddit) + '&size=' + str(limit) + '&filter=author,created_utc,full_link,id,num_comments,retrieved_on,score,selftext,title,url'
-    data = requests.get(url,headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}).text
+    url = 'https://api.pushshift.io/reddit/search/submission/?q=' + urllib.parse.quote_plus(searchQuery) + '&subreddit=' + urllib.parse.quote_plus(
+        subreddit) + '&size=' + str(limit) + '&filter=author,created_utc,full_link,id,num_comments,retrieved_on,score,selftext,title,url'
+
+    response = requests.get(url, headers={
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'})
+
+    data = response.text
 
     return data
-
-
 
 
 app.run(port=5000)
